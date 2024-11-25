a483f68a978aac73e40489a08dccc4b4a3677ab5
Guste Gaubaite <219.guste@gmail.com>
2024-11-25 14:13:50 +0100

    Starting exercise 2

--- Laboratorio Módulo 2 - Lenguajes/src/02-entregables/ejercicio-02.ts
+++ Laboratorio Módulo 2 - Lenguajes/src/02-entregables/ejercicio-02.ts
@@ -1,1 +1,21 @@
 console.log("************** DELIVERABLE 02 *********************");
+
+const arrayA = [1, 2, 3, 4];
+const arrayB = [5, 6, 7, 8, 9, 10];
+const arrayC = [11, 12, 13];
+const arrayD = [14, 15, 16, 17];
+
+// 2. Concat
+
+const concat = (a, b) => {
+  const concat = [...a, ...b];
+  console.log(concat);
+};
+
+concat(arrayA, arrayB);
+
+/*const concatMoreThanTwo = (...arrays) => {
+  console.log(arrays);
+};
+
+concatMoreThanTwo(arrayA, arrayB, arrayC, arrayD);*/
