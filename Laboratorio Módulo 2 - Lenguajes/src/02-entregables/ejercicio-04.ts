6833190682d8edcb6d9f4d75886705219db81b61
Guste Gaubaite <219.guste@gmail.com>
2024-11-26 23:17:03 +0100

    Read books exercise complete

--- Laboratorio Módulo 2 - Lenguajes/src/02-entregables/ejercicio-04.ts
+++ Laboratorio Módulo 2 - Lenguajes/src/02-entregables/ejercicio-04.ts
@@ -1,1 +1,22 @@
 console.log("************** DELIVERABLE 04 *********************");
+
+// 4. Read books
+
+const books = [
+  { title: "Harry Potter y la piedra filosofal", isRead: true },
+  { title: "Canción de hielo y fuego", isRead: false },
+  { title: "Devastación", isRead: true },
+];
+
+const isBookRead = (books, titleToSearch) => {
+  const foundBook = books.find((book) => book.title === titleToSearch);
+  if (!foundBook) {
+    return false;
+  }
+  const isRead = foundBook.isRead ? true : false;
+  return isRead;
+};
+
+console.log(isBookRead(books, "Devastación"));
+console.log(isBookRead(books, "Canción de hielo y fuego"));
+console.log(isBookRead(books, "Los Pilares de la Tierra"));
