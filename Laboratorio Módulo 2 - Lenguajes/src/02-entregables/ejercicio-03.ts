c985a4b4eafb16c7bd6dfaf330a80730e95f85df
Guste Gaubaite <219.guste@gmail.com>
2024-11-28 11:47:38 +0100

    Clone merge exercise complete

--- Laboratorio Módulo 2 - Lenguajes/src/02-entregables/ejercicio-03.ts
+++ Laboratorio Módulo 2 - Lenguajes/src/02-entregables/ejercicio-03.ts
@@ -1,1 +1,40 @@
 console.log("************** DELIVERABLE 03 *********************");
+
+// 3. Clone Merge
+
+console.log("**Clone**");
+
+const person1 = {
+  name: "Chloe",
+  surname: "Smith",
+  country: "UK",
+};
+
+const person2 = {
+  name: "Sarah",
+  age: 27,
+  occupation: "Web Developer",
+};
+
+const clone = (source) => {
+  const clone = { ...source };
+  return clone;
+};
+
+console.log(clone(person1));
+
+console.log("**Merge**");
+
+const merge = (source, target) => {
+  const a = clone(source);
+  const b = clone(target);
+
+  const merge = {
+    ...b,
+    ...a,
+  };
+
+  console.log(merge);
+};
+
+merge(person1, person2);
