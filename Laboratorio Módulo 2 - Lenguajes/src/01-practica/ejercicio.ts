efbaa858c72a2c18c61f6c6bb9c40fafb09449df
Guste Gaubaite <219.guste@gmail.com>
2024-11-28 15:40:00 +0100

    Complete biggest word exercise

--- Laboratorio Módulo 2 - Lenguajes/src/01-practica/ejercicio.ts
+++ Laboratorio Módulo 2 - Lenguajes/src/01-practica/ejercicio.ts
@@ -1,3 +1,21 @@
 console.log("************** PRACTICE *********************");
 console.log("Use this folder 00 practice to practice with homework exercises");
 console.log("You can add new files as long as they are imported from index.ts");
+
+console.log("#Biggest word");
+
+const biggestWord = (phrase: string): string => {
+  let longestWord = "";
+
+  const phraseWords = phrase.split(" ");
+
+  phraseWords.forEach((word) => {
+    if (word.length > longestWord.length) {
+      longestWord = word;
+    }
+  });
+  return longestWord;
+};
+
+console.log(biggestWord("Esta frase puede contener muchas palabras"));
+console.log(biggestWord("Ejercicios básicos de JavaScript"));
