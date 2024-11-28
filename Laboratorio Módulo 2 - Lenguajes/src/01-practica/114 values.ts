25340ee9a16996a1b7048403781643412ad2f8ff
Guste Gaubaite <219.guste@gmail.com>
2024-11-28 15:58:35 +0100

    Complete values exercise

--- Laboratorio Módulo 2 - Lenguajes/src/01-practica/114 values.ts
+++ Laboratorio Módulo 2 - Lenguajes/src/01-practica/114 values.ts
@@ -1,1 +1,23 @@
 console.log("#Values");
+
+const object = { id: 31, duration: 310, name: "long video", format: "mp4" };
+const userInfo = {
+  id: 2353,
+  username: "mwgbt",
+  password: "sdfsdKK124",
+  country: "Spain",
+};
+
+const values = (obj) => {
+  const listOfValues = [];
+
+  for (const key in obj) {
+    const value = obj[key];
+    listOfValues.push(value);
+  }
+
+  return listOfValues;
+};
+
+console.log(values(object));
+console.log(values(userInfo));
