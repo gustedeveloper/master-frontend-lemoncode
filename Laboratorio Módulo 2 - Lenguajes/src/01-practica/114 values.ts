5559f53fa4411ae9432b2f32bd8aaaf973f3bfd1
Guste Gaubaite <219.guste@gmail.com>
2024-11-29 17:45:03 +0100

    Improve typing of elements

--- Laboratorio Módulo 2 - Lenguajes/src/01-practica/114 values.ts
+++ Laboratorio Módulo 2 - Lenguajes/src/01-practica/114 values.ts
@@ -1,15 +1,37 @@
 console.log("#Values");
 
-const object = { id: 31, duration: 310, name: "long video", format: "mp4" };
-const userInfo = {
+interface Object {
+  id: number;
+  duration: number;
+  name: string;
+  format: string;
+}
+
+interface UserInfo {
+  id: number;
+  username: string;
+  password: string;
+  country: string;
+}
+
+const object: Object = {
+  id: 31,
+  duration: 310,
+  name: "long video",
+  format: "mp4",
+};
+const userInfo: UserInfo = {
   id: 2353,
   username: "mwgbt",
   password: "sdfsdKK124",
   country: "Spain",
 };
 
-const values = (obj) => {
-  const listOfValues = [];
+const values = (
+  obj: Object | UserInfo
+): Array<Object[keyof Object] | UserInfo[keyof UserInfo]> => {
+  const listOfValues: Array<Object[keyof Object] | UserInfo[keyof UserInfo]> =
+    [];
 
   for (const key in obj) {
     const value = obj[key];
