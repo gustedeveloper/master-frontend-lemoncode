4409b9dbc6dd45647bd27cfb658ac3cb799fa4d5
Guste Gaubaite <219.guste@gmail.com>
2024-12-04 12:21:38 +0100

    Add new exercise file

--- Laboratorio Módulo 2 - Lenguajes/src/index.ts
+++ Laboratorio Módulo 2 - Lenguajes/src/index.ts
@@ -1,7 +1,8 @@
 //import "./01-practica/101 biggest-word.js";
 //import "./01-practica/102 califications.js";
 //import "./01-practica/103 check-arguments.js";
-import "./01-practica/104 clone-merge.js"
+//import "./01-practica/104 clone-merge.js"
+import "./01-practica/105 deep-equal.js";
 /*import "./01-practica/114 values.js";
 import "./02-entregables/ejercicio-01.js";
 import "./02-entregables/ejercicio-02.js";
