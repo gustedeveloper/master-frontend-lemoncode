ea7d4b6fb3efb8ec52a32335bfd478cd4a7804a7
Guste Gaubaite <219.guste@gmail.com>
2024-12-04 12:06:15 +0100

    Add new exercise file

--- Laboratorio Módulo 2 - Lenguajes/src/index.ts
+++ Laboratorio Módulo 2 - Lenguajes/src/index.ts
@@ -1,11 +1,12 @@
-import "./01-practica/101 biggest-word.js";
-import "./01-practica/102 califications.js";
-import "./01-practica/114 values.js";
+//import "./01-practica/101 biggest-word.js";
+//import "./01-practica/102 califications.js";
+import "./01-practica/103 check-arguments.js";
+/*import "./01-practica/114 values.js";
 import "./02-entregables/ejercicio-01.js";
 import "./02-entregables/ejercicio-02.js";
 import "./02-entregables/ejercicio-03.js";
 import "./02-entregables/ejercicio-04.js";
 import "./02-entregables/ejercicio-05.js";
-import "./03-retos/ejercicio.js";
+import "./03-retos/ejercicio.js";*/
 
 console.log("Welcome to Exercises Playground.");
