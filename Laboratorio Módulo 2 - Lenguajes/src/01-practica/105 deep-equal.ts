a4a2c4a3bcf623512cf805c769ad227ab4d0f189
Guste Gaubaite <219.guste@gmail.com>
2024-12-04 12:22:05 +0100

    Deep equal A section complete

---
+++ Laboratorio Módulo 2 - Lenguajes/src/01-practica/105 deep-equal.ts
@@ -0,0 +1,22 @@
+console.log("#Deep equal");
+
+// Apartado A
+
+const user = { name: 'María', age: 30 };
+const clonedUser = { name: 'María', age: 30 };
+
+ function isEqual(a, b) {
+  const obj1 = Object.entries(a);
+  const obj2 = Object.entries(b);
+
+  if (obj1.length !== obj2.length) return false;
+
+  for (const key in a) {
+    if (!b.hasOwnProperty(key) || b[key] !== a[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isEqual(user, clonedUser)); // true
