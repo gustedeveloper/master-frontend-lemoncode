f82ab044ca01068128948c93e607213833cc4c5a
Guste Gaubaite <219.guste@gmail.com>
2024-12-22 12:26:33 +0100

    Curry set function and complete exercise

--- Laboratorio Módulo 2 - Lenguajes/src/01-practica/302 curry-setter.ts
+++ Laboratorio Módulo 2 - Lenguajes/src/01-practica/302 curry-setter.ts
@@ -13,3 +13,19 @@ const updatedJulia = set(julia, "age", 25);
 console.log(updatedJulia); // { name: 'Julia', surname: 'Álvarez', age: 25 }
 console.log(julia); // { name: 'Julia', surname: 'Álvarez', age: 19 }
 console.log(julia === updatedJulia); // false
+
+const currySet = (prop: string) => {
+  return (obj: object, value: string | number) => {
+    const newObj = { ...obj };
+    newObj[prop] = value;
+    return newObj;
+  };
+};
+
+const setName = currySet("name");
+const setSurName = currySet("surname");
+const setAge = currySet("age");
+
+console.log(setName(julia, "Ana")); // { name: 'Ana', surname: 'Álvarez', age: 19 };
+console.log(setSurName(julia, "González")); // { name: 'Julia', surname: 'González', age: 19 };
+console.log(setAge(julia, 25)); // { name: 'Julia', surname: 'Álvarez', age: 25 }
