537ed02fa250c802cf88ee2d2f02ac0ef07d7458
Guste Gaubaite <219.guste@gmail.com>
2025-01-06 11:17:40 +0100

    Add new exercise file

--- Laboratorio Módulo 2 - Lenguajes/src/index.ts
+++ Laboratorio Módulo 2 - Lenguajes/src/index.ts
@@ -21,6 +21,7 @@ import "./02-entregables/ejercicio-02.js";
 import "./02-entregables/ejercicio-03.js";
 import "./02-entregables/ejercicio-04.js";
 import "./02-entregables/ejercicio-05.js";*/
-import "./03-retos//console-traces.js";
+//import "./03-retos//console-traces.js";
+import "./03-retos/deep-access.js";
 
 console.log("Welcome to Exercises Playground.");
