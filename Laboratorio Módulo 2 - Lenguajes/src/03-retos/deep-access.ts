18875b3bc56bc369d2835b7b9f50f353650b4c10
Guste Gaubaite <219.guste@gmail.com>
2025-01-06 11:17:54 +0100

    Deep access first section complete

---
+++ Laboratorio Módulo 2 - Lenguajes/src/03-retos/deep-access.ts
@@ -0,0 +1,30 @@
+console.log("*Deep access challenge*");
+
+const myObject = {
+  a: 1,
+  b: {
+    c: null,
+    d: {
+      e: 3,
+      f: {
+        g: "bingo",
+      },
+    },
+  },
+};
+
+const deepGet = (obj, ...keys) => {
+  let current = obj;
+  for (const key of keys) {
+    if (current === null || !(key in current)) return undefined;
+    current = current[key];
+  }
+  return current;
+};
+
+console.log(deepGet(myObject, "x")); // undefined
+console.log(deepGet(myObject, "a")); // 1
+console.log(deepGet(myObject, "b")); // { c: null, d: {....}}
+console.log(deepGet(myObject, "b", "c")); // null
+console.log(deepGet(myObject, "b", "d", "f", "g")); // bingo
+console.log(deepGet(myObject)); // {a: 1, b: {...}}
