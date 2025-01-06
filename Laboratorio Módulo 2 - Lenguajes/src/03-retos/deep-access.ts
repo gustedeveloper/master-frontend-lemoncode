b865999730f1099d4be04bdbc1bd6ec5029602f5
Guste Gaubaite <219.guste@gmail.com>
2025-01-06 12:42:12 +0100

    Section B complete

--- Laboratorio Módulo 2 - Lenguajes/src/03-retos/deep-access.ts
+++ Laboratorio Módulo 2 - Lenguajes/src/03-retos/deep-access.ts
@@ -1,5 +1,7 @@
 console.log("*Deep access challenge*");
 
+console.log("Section A - get");
+
 const myObject = {
   a: 1,
   b: {
@@ -28,3 +30,33 @@ console.log(deepGet(myObject, "b")); // { c: null, d: {....}}
 console.log(deepGet(myObject, "b", "c")); // null
 console.log(deepGet(myObject, "b", "d", "f", "g")); // bingo
 console.log(deepGet(myObject)); // {a: 1, b: {...}}
+
+console.log("Section B - set");
+
+const myObject2 = {};
+const myObject3 = {};
+
+const deepSet = (value, obj, ...keys) => {
+  let current = obj;
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
+
+    if (i === keys.length - 1) {
+      current[key] = value;
+    } else {
+      if (!current[key]) {
+        current[key] = {};
+      }
+    }
+    current = current[key];
+  }
+};
+
+deepSet(1, myObject2, "a", "b");
+console.log(JSON.stringify(myObject2)); // {a: { b: 1}}
+deepSet(2, myObject2, "a", "c");
+console.log(JSON.stringify(myObject2)); // {a: { b: 1, c: 2}}
+deepSet(3, myObject2, "a");
+console.log(JSON.stringify(myObject2)); // {a: 3}
+deepSet(4, myObject2);
+console.log(JSON.stringify(myObject2)); // Do nothing // {a: 3}
