a49f9f7a8c672c1d4519e60574c6e55b5b2add80
Guste Gaubaite <219.guste@gmail.com>
2025-01-07 11:19:38 +0100

    Optimized memoize function

--- Laboratorio Módulo 2 - Lenguajes/src/03-retos/memoization.ts
+++ Laboratorio Módulo 2 - Lenguajes/src/03-retos/memoization.ts
@@ -1,3 +1,5 @@
+// Section A
+
 const expensiveFunction = (): number => {
   console.log("Una única llamada");
   return 3.1415;
@@ -19,3 +21,15 @@ const memoized = memoize(expensiveFunction);
 console.log(memoized()); // Una única llamada // 3.1415
 console.log(memoized()); // 3.1415
 console.log(memoized()); // 3.1415
+
+// Section B
+
+const memoizeOneLine = <T>(f: () => T): T => {
+  let cachedResult: T | undefined;
+  return cachedResult ?? (cachedResult = f());
+};
+
+const memoized2 = memoizeOneLine(expensiveFunction);
+console.log(memoized2);
+console.log(memoized2);
+console.log(memoized2);
