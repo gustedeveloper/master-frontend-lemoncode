d27852d498079983fcefbb3df8e0afa2759f2ad0
Guste Gaubaite <219.guste@gmail.com>
2025-01-18 18:37:43 +0100

    Add types to dices exercise

--- Laboratorio Módulo 2 - Lenguajes/src/01-practica/106 dices.ts
+++ Laboratorio Módulo 2 - Lenguajes/src/01-practica/106 dices.ts
@@ -1,42 +1,49 @@
 console.log("#Dices");
 
-const playDices = () => {
-    let dice1, dice2;
-  
-    const random = () => Math.floor(Math.random() * 6 + 1);
-  
-    return {
-      reset: () => {
-        dice1 = undefined;
-        dice2 = undefined;
-        console.log("Dices' values are reset!");
-      },
-      getDices: () => {
-        dice1 = random();
-        dice2 = random();
-        console.log('You have rolled the dices!');
-      },
-      checkResult: () => {
-        if (dice1 === 6 && dice2 === 6) {
-          console.log("Congrats! You've got 2 sixs :)");
-        } else {
-          console.log(
-            `You've got ${dice1} and ${dice2}. Reset the game and try again!`
-          );
-        }
-      },
-      checkDices: () => {
-        console.log(`Current dices values: ${dice1}, ${dice2}`);
-      },
-    };
-  };
-  
-  const game = playDices();
+type DiceGame = {
+  reset: () => void;
+  getDices: () => void;
+  checkResult: () => void;
+  checkDices: () => void;
+};
 
-  game.getDices();
-  game.checkDices();
-  game.getDices();
-  game.checkResult();
-  game.reset();
-  game.getDices();
-  game.checkResult();
+const playDices = (): DiceGame => {
+  let dice1: number | undefined, dice2: number | undefined;
+
+  const random = (): number => Math.floor(Math.random() * 6 + 1);
+
+  return {
+    reset: (): void => {
+      dice1 = undefined;
+      dice2 = undefined;
+      console.log("Dices' values are reset!");
+    },
+    getDices: (): void => {
+      dice1 = random();
+      dice2 = random();
+      console.log("You have rolled the dices!");
+    },
+    checkResult: (): void => {
+      if (dice1 === 6 && dice2 === 6) {
+        console.log("Congrats! You've got 2 sixs :)");
+      } else {
+        console.log(
+          `You've got ${dice1} and ${dice2}. Reset the game and try again!`
+        );
+      }
+    },
+    checkDices: (): void => {
+      console.log(`Current dices values: ${dice1}, ${dice2}`);
+    },
+  };
+};
+
+const game = playDices();
+
+game.getDices();
+game.checkDices();
+game.getDices();
+game.checkResult();
+game.reset();
+game.getDices();
+game.checkResult();
