35b67a45acab4b6bbb80f7cbeae95f5bb913d65b
Guste Gaubaite <219.guste@gmail.com>
2025-01-19 17:13:48 +0100

    Create MainComponent with user input formatting and personalized welcome message

--- Laboratorio Módulo 3 - Bundling/laboratorio-modulo-3-bundling/src/main-component.jsx
+++ Laboratorio Módulo 3 - Bundling/laboratorio-modulo-3-bundling/src/main-component.jsx
@@ -1,15 +1,33 @@
 import React from "react";
 
 export const MainComponent = () => {
-  const [message, setMessage] = React.useState();
+  const [user, setUser] = React.useState("User");
+  const [inputValue, setInputValue] = React.useState("");
 
-  React.useEffect(() => {
-    setMessage();
-  }, []);
+  const formatValue = (value) => {
+    const [firstLetter, ...rest] = value.trim();
+    return firstLetter.toUpperCase() + rest.join("").toLowerCase();
+  };
+
+  const handleClick = () => {
+    if (inputValue.trim() !== "") {
+      setUser(formatValue(inputValue));
+      setInputValue("");
+    }
+  };
 
   return (
     <div>
-      <h1>Hello world with React!</h1>
+      <h1>{user}, welcome to this world made by React!</h1>
+
+      <label htmlFor="userInput">Please, enter your name</label>
+
+      <input
+        id="userInput"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={handleClick}>Send</button>
     </div>
   );
 };
