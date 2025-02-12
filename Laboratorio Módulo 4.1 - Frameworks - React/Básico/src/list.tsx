1928aa233bfaa8caa101b2d317c6ba3006429f6b
Guste Gaubaite <219.guste@gmail.com>
2025-02-12 19:00:41 +0100

    Implement functionality to search and get members' information of different organizations, establishing by default lemoncode members

--- Laboratorio Módulo 4.1 - Frameworks - React/Básico/src/list.tsx
+++ Laboratorio Módulo 4.1 - Frameworks - React/Básico/src/list.tsx
@@ -9,16 +9,27 @@ interface MemberEntity {
 
 export const ListPage: React.FC = () => {
   const [members, setMembers] = React.useState<MemberEntity[]>([]);
+  const [value, setValue] = React.useState("lemoncode");
+  const [organization, setOrganization] = React.useState("lemoncode");
 
   React.useEffect(() => {
-    fetch(`https://api.github.com/orgs/lemoncode/members`)
+    fetch(`https://api.github.com/orgs/${organization}/members`)
       .then((response) => response.json())
       .then((json) => setMembers(json));
-  }, []);
+  }, [organization]);
+
+  const handleSearchButton = () => {
+    setOrganization(value);
+  };
 
   return (
     <>
-      <h2>Hello from List page</h2>+{" "}
+      <h2>Hello from List page</h2>
+      <div>
+        <label>Enter organization name: </label>
+        <input value={value} onChange={(e) => setValue(e.target.value)}></input>
+        <button onClick={() => handleSearchButton()}>Search</button>
+      </div>
       <div className="list-user-list-container">
         <span className="list-header">Avatar</span>
         <span className="list-header">Id</span>
