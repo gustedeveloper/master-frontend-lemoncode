3c13c736c08a58b6c6cd49f45ba3a712eb587063
Guste Gaubaite <219.guste@gmail.com>
2025-02-17 11:30:25 +0100

    Implement webpack-bundle-analyzer for bundle size analysis and add splitChunks optimization to separate vendor code in production build

--- Laboratorio Módulo 3 - Bundling/webpack/webpack.prod.js
+++ Laboratorio Módulo 3 - Bundling/webpack/webpack.prod.js
@@ -9,6 +9,26 @@ export default merge(common, {
     filename: "[name].[chunkhash].js",
     clean: true,
   },
+  optimization: {
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          chunks: "all",
+          name: (module) => {
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            )?.[1];
+            return packageName
+              ? `vendor/${packageName.replace("@", "")}`
+              : null;
+          },
+          test: /[\\/]node_modules[\\/]/,
+          enforce: true,
+        },
+      },
+    },
+  },
   module: {
     rules: [
       {
