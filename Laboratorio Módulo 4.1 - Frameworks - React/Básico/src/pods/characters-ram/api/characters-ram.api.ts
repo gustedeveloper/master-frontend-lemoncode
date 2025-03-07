d99f908c8fc9264df06c9d52353db53f72e9cd43
Guste Gaubaite <219.guste@gmail.com>
2025-03-07 17:51:09 +0100

    Establish random page when getting characters from api

--- Laboratorio Módulo 4.1 - Frameworks - React/Básico/src/pods/characters-ram/api/characters-ram.api.ts
+++ Laboratorio Módulo 4.1 - Frameworks - React/Básico/src/pods/characters-ram/api/characters-ram.api.ts
@@ -1,6 +1,8 @@
 import { CharacterApi } from "./characters-ram.api-model";
 
-export const getCharactersCollection = (): Promise<CharacterApi[]> =>
-  fetch(`https://rickandmortyapi.com/api/character`)
+export const getCharactersCollection = (): Promise<CharacterApi[]> => {
+  const randomPage = Math.floor(Math.random() * 42) + 1;
+  return fetch(`https://rickandmortyapi.com/api/character?page=${randomPage}`)
     .then((response) => response.json())
     .then((data) => data.results);
+};
