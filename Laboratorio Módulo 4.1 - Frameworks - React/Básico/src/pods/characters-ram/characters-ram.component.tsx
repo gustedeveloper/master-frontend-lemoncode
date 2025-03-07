0729b3b729d04b4948f3f24fc69badd76871e1ea
Guste Gaubaite <219.guste@gmail.com>
2025-03-07 18:29:44 +0100

    Add window scroll to top button and adjust grids and paddings

--- Laboratorio Módulo 4.1 - Frameworks - React/Básico/src/pods/characters-ram/characters-ram.component.tsx
+++ Laboratorio Módulo 4.1 - Frameworks - React/Básico/src/pods/characters-ram/characters-ram.component.tsx
@@ -19,6 +19,11 @@ interface Props {
 
 export const CharactersRAMComponent: React.FC<Props> = (props) => {
   const { value, setValue, characters, setPage } = props;
+
+  const handleScrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <>
       <Grid2
@@ -30,9 +35,13 @@ export const CharactersRAMComponent: React.FC<Props> = (props) => {
           },
         }}
       >
-        <Grid2 size={12} sx={{ display: "flex", justifyContent: "center" }}>
-          <Grid2 container sx={{ padding: "40px 0px 20px 0px" }}>
+        <Grid2 size={12}>
+          <Grid2
+            container
+            sx={{ padding: "40px 0px 20px 0px", justifyContent: "center" }}
+          >
             <TextField
+              sx={{ maxWidth: "200px" }}
               id="filled-basic"
               label="Enter character"
               variant="filled"
@@ -41,106 +50,114 @@ export const CharactersRAMComponent: React.FC<Props> = (props) => {
               onChange={(e) => setValue(e.target.value)}
             />
           </Grid2>
-        </Grid2>
 
-        <Grid2
-          size={12}
-          sx={{
-            display: "flex",
-            flexWrap: "wrap",
-            gap: "30px",
-            justifyContent: "space-evenly",
-          }}
-        >
-          {characters.map((character) => (
-            <Grid2 container key={character.id}>
-              <Grid2 size={{ xs: 12, sm: 6 }}>
-                <Card
-                  sx={{
-                    flexDirection: {
-                      xs: "column",
-                      md: "row",
-                    },
-                    width: {
-                      xs: "300px",
-                      md: "550px",
-                    },
-                    height: {
-                      xs: "530px",
-                      md: "200px",
-                    },
-                    borderRadius: "5px",
-                    display: "flex",
-                    bgcolor: "#8d99ae",
-                  }}
-                >
-                  <CardMedia
-                    component="img"
+          <Grid2
+            size={12}
+            sx={{
+              display: "flex",
+              flexWrap: "wrap",
+              gap: "30px",
+              justifyContent: "space-evenly",
+            }}
+          >
+            {characters.map((character) => (
+              <Grid2 container key={character.id}>
+                <Grid2 size={{ xs: 12, sm: 6 }}>
+                  <Card
                     sx={{
+                      flexDirection: {
+                        xs: "column",
+                        md: "row",
+                      },
                       width: {
-                        sx: 300,
-                        md: 200,
+                        xs: "300px",
+                        md: "550px",
                       },
-                      borderRadius: {
-                        sx: "0px 5px 5px 0px",
-                        md: "5px 0px 0px 5px",
+                      height: {
+                        xs: "530px",
+                        md: "200px",
                       },
-                    }}
-                    image={character.image}
-                    alt={character.name}
-                  ></CardMedia>
-                  <CardContent
-                    sx={{
+                      borderRadius: "5px",
                       display: "flex",
-                      flex: "1 0 auto",
-                      flexDirection: "column",
-                      justifyContent: "space-between",
+                      bgcolor: "#8d99ae",
                     }}
                   >
-                    <Typography variant="h3" sx={{ color: "white" }}>
-                      {character.name}
-                    </Typography>
-                    <Typography
+                    <CardMedia
+                      component="img"
                       sx={{
-                        color: "white",
-                        fontWeight: "bold",
-                        fontSize: "14px",
-                        mb: 1.25,
+                        width: {
+                          sx: 300,
+                          md: 200,
+                        },
+                        borderRadius: {
+                          sx: "0px 5px 5px 0px",
+                          md: "5px 0px 0px 5px",
+                        },
+                      }}
+                      image={character.image}
+                      alt={character.name}
+                    ></CardMedia>
+                    <CardContent
+                      sx={{
+                        display: "flex",
+                        flex: "1 0 auto",
+                        flexDirection: "column",
+                        justifyContent: "space-between",
                       }}
                     >
-                      {character.status} - {character.species}
-                    </Typography>
+                      <Typography variant="h3" sx={{ color: "white" }}>
+                        {character.name}
+                      </Typography>
+                      <Typography
+                        sx={{
+                          color: "white",
+                          fontWeight: "bold",
+                          fontSize: "14px",
+                          mb: 1.25,
+                        }}
+                      >
+                        {character.status} - {character.species}
+                      </Typography>
 
-                    <Typography sx={{ color: "#B9CAE7", fontWeight: "bold" }}>
-                      Last known location:
-                    </Typography>
-                    <Typography sx={{ color: "white", mb: 1.25 }}>
-                      {character.currentLocation.name}
-                    </Typography>
+                      <Typography sx={{ color: "#B9CAE7", fontWeight: "bold" }}>
+                        Last known location:
+                      </Typography>
+                      <Typography sx={{ color: "white", mb: 1.25 }}>
+                        {character.currentLocation.name}
+                      </Typography>
 
-                    <Typography sx={{ color: "#B9CAE7", fontWeight: "bold" }}>
-                      First seen in:
-                    </Typography>
-                    <Typography sx={{ color: "white" }}>
-                      {character.origin.name}
-                    </Typography>
-                  </CardContent>
-                </Card>
+                      <Typography sx={{ color: "#B9CAE7", fontWeight: "bold" }}>
+                        First seen in:
+                      </Typography>
+                      <Typography sx={{ color: "white" }}>
+                        {character.origin.name}
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </Grid2>
               </Grid2>
+            ))}
+          </Grid2>
+
+          <Grid2 container spacing={3} sx={{ p: "30px" }}>
+            <Grid2
+              size={6}
+              sx={{ display: "flex", justifyContent: "flex-end" }}
+            >
+              <Button
+                sx={{ p: "20px" }}
+                variant="contained"
+                onClick={() => setPage((prev) => prev + 1)}
+              >
+                Load more
+              </Button>
             </Grid2>
-          ))}
-        </Grid2>
-        <Grid2
-          size={12}
-          sx={{ display: "flex", justifyContent: "center", p: "50px" }}
-        >
-          <Button
-            sx={{ p: "20px" }}
-            variant="contained"
-            onClick={() => setPage((prev) => prev + 1)}
-          >
-            Load more
-          </Button>
+            <Grid2 size={6}>
+              <Button sx={{ p: "20px" }} onClick={handleScrollToTop}>
+                Go to the top
+              </Button>
+            </Grid2>
+          </Grid2>
         </Grid2>
       </Grid2>
     </>
