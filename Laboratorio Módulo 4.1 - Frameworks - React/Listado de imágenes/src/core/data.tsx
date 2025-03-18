6d3cc1a9a9d4c4e1acf27e903b32c638b0996223
Guste Gaubaite <219.guste@gmail.com>
2025-03-18 18:12:19 +0100

    Create mock data of pictures

---
+++ Laboratorio Módulo 4.1 - Frameworks - React/Listado de imágenes/src/core/data.tsx
@@ -0,0 +1,90 @@
+import { PictureInfo } from "./model";
+
+const landscapeSrc = "src/assets/landscapes/landscape";
+const citySrc = "src/assets/city/city";
+
+export const landscapePictures: PictureInfo[] = [
+  {
+    id: "ls1",
+    picUrl: `${landscapeSrc}1.jpg`,
+    title: "Makeshift dock on a lake in Finland",
+  },
+  {
+    id: "ls2",
+    picUrl: `${landscapeSrc}2.jpg`,
+    title: "Lake in Nuuksio's Park",
+  },
+  {
+    id: "ls3",
+    picUrl: `${landscapeSrc}3.jpg`,
+    title: "Panoramic view of a lake among trees",
+  },
+  {
+    id: "ls4",
+    picUrl: `${landscapeSrc}4.jpg`,
+    title: "Rustic stairs in nature",
+  },
+  {
+    id: "ls5",
+    picUrl: `${landscapeSrc}5.jpg`,
+    title: "Natural path in the woods",
+  },
+  {
+    id: "ls6",
+    picUrl: `${landscapeSrc}6.jpg`,
+    title: "In the middle of the forest",
+  },
+  {
+    id: "ls7",
+    picUrl: `${landscapeSrc}7.jpg`,
+    title: "Between the woods",
+  },
+  {
+    id: "ls8",
+    picUrl: `${landscapeSrc}8.jpg`,
+    title: "Makeshift footbridge in the middle of a swamp",
+  },
+];
+
+export const cityPictures: PictureInfo[] = [
+  {
+    id: "c1",
+    picUrl: `${citySrc}1.jpg`,
+    title: "Helsinki Cathedral",
+  },
+  {
+    id: "c2",
+    picUrl: `${citySrc}1.jpg`,
+    title: "Helsinki Cathedral",
+  },
+  {
+    id: "c3",
+    picUrl: `${citySrc}1.jpg`,
+    title: "Helsinki Cathedral",
+  },
+  {
+    id: "c4",
+    picUrl: `${citySrc}1.jpg`,
+    title: "Helsinki Cathedral",
+  },
+  {
+    id: "c5",
+    picUrl: `${citySrc}1.jpg`,
+    title: "Helsinki Cathedral",
+  },
+  {
+    id: "c6",
+    picUrl: `${citySrc}1.jpg`,
+    title: "Helsinki Cathedral",
+  },
+  {
+    id: "c7",
+    picUrl: `${citySrc}1.jpg`,
+    title: "Helsinki Cathedral",
+  },
+  {
+    id: "c8",
+    picUrl: `${citySrc}1.jpg`,
+    title: "Helsinki Cathedral",
+  },
+];
