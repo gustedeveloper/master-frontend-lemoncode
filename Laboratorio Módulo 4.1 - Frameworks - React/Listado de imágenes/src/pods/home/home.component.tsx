fb368c956d42ae3b6224b69a58caeef1eb8dc5ea
Guste Gaubaite <219.guste@gmail.com>
2025-03-18 19:21:25 +0100

    Implement basic navigation

--- Laboratorio Módulo 4.1 - Frameworks - React/Listado de imágenes/src/pods/home/home.component.tsx
+++ Laboratorio Módulo 4.1 - Frameworks - React/Listado de imágenes/src/pods/home/home.component.tsx
@@ -1,7 +1,16 @@
+import { FC } from "react";
+import { useNavigate } from "react-router-dom";
+
 export const HomeComponent: FC = () => {
+  const navigate = useNavigate();
+
   return (
     <>
       <h1>Home page</h1>
+      <button onClick={() => navigate("/city-category")}>City category</button>
+      <button onClick={() => navigate("/landscape-category")}>
+        Landscape category
+      </button>
     </>
   );
 };
