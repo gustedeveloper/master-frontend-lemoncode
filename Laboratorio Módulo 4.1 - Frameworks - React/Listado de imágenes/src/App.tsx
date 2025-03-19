90474109043b0c65f03a634c478a49acd35d6698
Guste Gaubaite <219.guste@gmail.com>
2025-03-19 18:14:45 +0100

    Wrap app with pictures context provider

--- Laboratorio Módulo 4.1 - Frameworks - React/Listado de imágenes/src/App.tsx
+++ Laboratorio Módulo 4.1 - Frameworks - React/Listado de imágenes/src/App.tsx
@@ -1,9 +1,10 @@
 import { AppRoutes } from "./routes/app-routes";
+import { PicturesProvider } from "./core/context/pictures-provider";
 
 export const App = () => {
   return (
-    <>
+    <PicturesProvider>
       <AppRoutes />
-    </>
+    </PicturesProvider>
   );
 };
