5a4ce90b984de9ac60eb0908a7bc33f7944214e0
Guste Gaubaite <219.guste@gmail.com>
2025-03-26 14:29:17 +0100

    Adjust components size

--- Laboratorio Módulo 4.1 - Frameworks - React/Listado de imágenes/src/layout/app.layout.tsx
+++ Laboratorio Módulo 4.1 - Frameworks - React/Listado de imágenes/src/layout/app.layout.tsx
@@ -14,7 +14,7 @@ export const AppLayout: FC<Props> = ({ children }) => (
       container
       sx={{
         mt: {
-          xs: "145px",
+          xs: "14px",
           md: "65px",
         },
         justifyContent: "space-evenly",
@@ -31,12 +31,8 @@ export const AppLayout: FC<Props> = ({ children }) => (
             },
           }}
         >
-          <Grid2 size={{ xs: 6, sm: 8, md: 7.5, lg: 9, xl: 10 }}>
-            {children[0]}
-          </Grid2>
-          <Grid2 size={{ xs: 6, sm: 4, md: 4.5, lg: 3, xl: 2 }}>
-            {children[1]}
-          </Grid2>
+          <Grid2 size={{ xs: 6, sm: 8, md: 7.5, lg: 9 }}>{children[0]}</Grid2>
+          <Grid2 size={{ xs: 6, sm: 4, md: 4.5, lg: 3 }}>{children[1]}</Grid2>
         </Grid2>
       </Grid2>
     </Grid2>
