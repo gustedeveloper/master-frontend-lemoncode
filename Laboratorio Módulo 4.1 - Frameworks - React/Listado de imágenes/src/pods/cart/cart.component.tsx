df5949a2cec0f103ed115d539a6e147c2df6fcd9
Guste Gaubaite <219.guste@gmail.com>
2025-03-26 09:56:28 +0100

    Style cart component with MUI

--- Laboratorio Módulo 4.1 - Frameworks - React/Listado de imágenes/src/pods/cart/cart.component.tsx
+++ Laboratorio Módulo 4.1 - Frameworks - React/Listado de imágenes/src/pods/cart/cart.component.tsx
@@ -1,5 +1,15 @@
 import { FC } from "react";
 import { PictureInfo } from "../../core/model";
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid2,
+  Typography,
+} from "@mui/material";
 
 interface Props {
   cartPictures: PictureInfo[];
@@ -10,19 +20,51 @@ export const CartComponent: FC<Props> = (props) => {
   const { cartPictures, deleteFromCart } = props;
 
   return (
-    <>
-      <div className="cart">
-        <h1>Shopping Cart</h1>
-        <div className="img-container">
-          {cartPictures.map((picture) => (
-            <div key={picture.id} className="pic-container">
-              <img className="cart-img" src={picture.picUrl} />
-              <span>{picture.title}</span>
-              <button onClick={() => deleteFromCart(picture.id)}>Delete</button>
-            </div>
-          ))}
-        </div>
-      </div>
-    </>
+    <Grid2 container>
+      <Typography variant="h3" sx={{ p: "20px 0px" }}>
+        Shopping Cart
+      </Typography>
+      <Grid2
+        size={12}
+        sx={{ display: "flex", flexDirection: "column", gap: "20px" }}
+      >
+        {cartPictures.map((picture) => (
+          <Grid2 container key={picture.id}>
+            <Grid2 sx={{ display: "flex" }}>
+              <Card sx={{ width: "130px" }}>
+                <CardMedia
+                  component="img"
+                  image={picture.picUrl}
+                  alt={picture.title}
+                ></CardMedia>
+              </Card>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                }}
+              >
+                <CardContent sx={{ p: "5px 20px" }}>
+                  <Typography sx={{ fontSize: "12px" }}>
+                    {picture.title}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button
+                    variant="contained"
+                    size="small"
+                    sx={{ ml: "20px" }}
+                    onClick={() => deleteFromCart(picture.id)}
+                  >
+                    Delete
+                  </Button>
+                </CardActions>
+              </Box>
+            </Grid2>
+          </Grid2>
+        ))}
+      </Grid2>
+    </Grid2>
   );
 };
