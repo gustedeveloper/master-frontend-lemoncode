1787922210e76741ed5d6358fb5fab2db9738e7c
Guste Gaubaite <219.guste@gmail.com>
2025-03-27 19:36:58 +0100

    Style summary element

--- Laboratorio Módulo 4.1 - Frameworks - React/Listado de imágenes/src/pods/checkout/checkout.component.tsx
+++ Laboratorio Módulo 4.1 - Frameworks - React/Listado de imágenes/src/pods/checkout/checkout.component.tsx
@@ -1,36 +1,48 @@
 import { Button, Grid2, Typography } from "@mui/material";
 import { FC } from "react";
 
-export const CheckoutComponent: FC = () => {
+interface Props {
+  totalCartBalance: number;
+}
+
+export const CheckoutComponent: FC<Props> = (props) => {
+  const { totalCartBalance } = props;
+
   return (
     <Grid2
       container
       sx={{
         p: "50px",
         width: "415px",
-        height: "600px",
+        height: "400px",
         bgcolor: "secondary.light",
         flexDirection: "column",
-        justifyContent: "flex-start",
+        justifyContent: "space-between",
       }}
     >
-      <Grid2 size={12}>
-        <Grid2 container sx={{ flexDirection: "column" }}>
-          <Grid2 size={4}>
-            <Typography
-              variant="h3"
-              sx={{ fontWeight: "lighter", color: "primary.main" }}
-            >
-              Summary
-            </Typography>
-          </Grid2>
-          <Grid2 size={4}>
-            <Typography>Cart Total</Typography>
-          </Grid2>
-          <Grid2 size={4}>
-            <Button variant="contained">Checkout</Button>
-          </Grid2>
-        </Grid2>
+      <Grid2>
+        <Typography
+          variant="h3"
+          sx={{ fontWeight: "lighter", color: "primary.main" }}
+        >
+          Summary
+        </Typography>
+      </Grid2>
+
+      <Grid2 sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Typography>Balance</Typography>
+        <Typography
+          variant="h3"
+          sx={{ fontWeight: "lighter", fontStyle: "oblique", pr: "50px" }}
+        >
+          {totalCartBalance.toFixed(2)} €
+        </Typography>
+      </Grid2>
+
+      <Grid2 sx={{ display: "flex", justifyContent: "center" }}>
+        <Button size="large" variant="contained">
+          Checkout
+        </Button>
       </Grid2>
     </Grid2>
   );
--- Laboratorio Módulo 4.1 - Frameworks - React/Listado de imágenes/src/pods/checkout/checkout.container.tsx
+++ Laboratorio Módulo 4.1 - Frameworks - React/Listado de imágenes/src/pods/checkout/checkout.container.tsx
@@ -1,6 +1,9 @@
-import { FC } from "react";
+import { FC, useContext } from "react";
 import { CheckoutComponent } from "./checkout.component";
+import { PicturesContext } from "../../core/context/pictures-context";
 
 export const CheckoutContainer: FC = () => {
-  return <CheckoutComponent />;
+  const { totalCartBalance } = useContext(PicturesContext);
+
+  return <CheckoutComponent totalCartBalance={totalCartBalance} />;
 };
