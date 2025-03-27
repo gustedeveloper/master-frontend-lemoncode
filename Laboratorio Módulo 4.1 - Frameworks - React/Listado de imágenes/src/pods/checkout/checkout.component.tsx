d45e966e98810b94404aa93cfaf7030deb54983a
Guste Gaubaite <219.guste@gmail.com>
2025-03-27 12:39:09 +0100

    Move cart container to checkout page, remove it from checkout component, maintaining the correct POD arquitecture

--- Laboratorio Módulo 4.1 - Frameworks - React/Listado de imágenes/src/pods/checkout/checkout.component.tsx
+++ Laboratorio Módulo 4.1 - Frameworks - React/Listado de imágenes/src/pods/checkout/checkout.component.tsx
@@ -1,12 +1,10 @@
 import { Typography } from "@mui/material";
 import { FC } from "react";
-import { CartContainer } from "../cart";
 
 export const CheckoutComponent: FC = () => {
   return (
     <>
       <Typography>Checkout</Typography>
-      <CartContainer />
     </>
   );
 };
--- Laboratorio Módulo 4.1 - Frameworks - React/Listado de imágenes/src/scenes/checkout.tsx
+++ Laboratorio Módulo 4.1 - Frameworks - React/Listado de imágenes/src/scenes/checkout.tsx
@@ -1,12 +1,14 @@
 import { FC } from "react";
 import { AppLayout } from "../layout";
 import { CheckoutContainer } from "../pods/checkout";
+import { CartContainer } from "../pods/cart";
 
 export const CheckoutPage: FC = () => {
   return (
     <>
       <AppLayout>
         <CheckoutContainer />
+        <CartContainer />
       </AppLayout>
     </>
   );
