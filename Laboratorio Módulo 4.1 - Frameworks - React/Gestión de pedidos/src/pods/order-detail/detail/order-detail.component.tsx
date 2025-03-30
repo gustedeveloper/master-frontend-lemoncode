91fc4eaa01af7b4b606bd2abc20e8ef68426c417
Guste Gaubaite <219.guste@gmail.com>
2025-03-30 20:19:45 +0200

    Implement order status percentage display

--- Laboratorio Módulo 4.1 - Frameworks - React/Gestión de pedidos/src/pods/order-detail/detail/order-detail.component.tsx
+++ Laboratorio Módulo 4.1 - Frameworks - React/Gestión de pedidos/src/pods/order-detail/detail/order-detail.component.tsx
@@ -4,10 +4,11 @@ import { FC } from "react";
 
 interface Props {
   selectedOrder?: Order;
+  statusPercentage: number;
 }
 
 export const OrderDetailComponent: FC<Props> = (props) => {
-  const { selectedOrder } = props;
+  const { selectedOrder, statusPercentage } = props;
 
   return (
     <Box sx={{ m: "50px", width: "1000px", display: "flex", gap: "30px" }}>
@@ -17,7 +18,7 @@ export const OrderDetailComponent: FC<Props> = (props) => {
       ></TextField>
       <TextField
         sx={{ width: "100px" }}
-        value={selectedOrder?.status}
+        value={`${Math.round(statusPercentage)}%`}
         label="Status"
       ></TextField>
       <Button variant="contained">Send</Button>
--- Laboratorio Módulo 4.1 - Frameworks - React/Gestión de pedidos/src/pods/order-detail/detail/order-detail.container.tsx
+++ Laboratorio Módulo 4.1 - Frameworks - React/Gestión de pedidos/src/pods/order-detail/detail/order-detail.container.tsx
@@ -1,9 +1,32 @@
-import { FC, useContext } from "react";
+import { FC, useContext, useEffect, useState } from "react";
 import { OrderDetailComponent } from "./order-detail.component";
 import { OrdersContext } from "@/core/context/orders-context";
 
 export const OrderDetailContainer: FC = () => {
   const { selectedOrder } = useContext(OrdersContext);
+  const [statusPercentage, setStatusPercentage] = useState<number>(0);
 
-  return <OrderDetailComponent selectedOrder={selectedOrder} />;
+  const getStatusPercentage = () => {
+    const items = selectedOrder?.items;
+    let validatedItems = 0;
+    if (items) {
+      items.forEach((item) => {
+        if (item.status) validatedItems += 1;
+      });
+
+      return (validatedItems / items.length) * 100;
+    }
+  };
+
+  useEffect(() => {
+    const percentage = getStatusPercentage();
+    if (percentage) setStatusPercentage(percentage);
+  }, [selectedOrder]);
+
+  return (
+    <OrderDetailComponent
+      selectedOrder={selectedOrder}
+      statusPercentage={statusPercentage}
+    />
+  );
 };
