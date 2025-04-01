4fee4f204639e3de871e283aa59dfa698456f43e
Guste Gaubaite <219.guste@gmail.com>
2025-04-01 15:48:15 +0200

    Disable order detail info text fields and button if all order items are not validated

--- Laboratorio Módulo 4.1 - Frameworks - React/Gestión de pedidos/src/pods/order-detail/detail/order-detail.component.tsx
+++ Laboratorio Módulo 4.1 - Frameworks - React/Gestión de pedidos/src/pods/order-detail/detail/order-detail.component.tsx
@@ -13,15 +13,22 @@ export const OrderDetailComponent: FC<Props> = (props) => {
   return (
     <Box sx={{ m: "50px", width: "1000px", display: "flex", gap: "30px" }}>
       <TextField
+        disabled
         value={selectedOrder?.totalAmount}
         label="Total amount"
       ></TextField>
       <TextField
+        disabled
         sx={{ width: "100px" }}
         value={`${Math.round(statusPercentage)}%`}
         label="Status"
       ></TextField>
-      <Button variant="contained">Send</Button>
+      <Button
+        variant="contained"
+        disabled={!selectedOrder?.items.every((item) => item.status === true)}
+      >
+        Send
+      </Button>
     </Box>
   );
 };
