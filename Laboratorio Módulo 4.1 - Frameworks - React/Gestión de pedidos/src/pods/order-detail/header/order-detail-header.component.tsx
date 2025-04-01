efb2ca42867b0297872d8d397712fc3015bef1f8
Guste Gaubaite <219.guste@gmail.com>
2025-04-01 21:01:30 +0200

    Fix uncontrolled to controlled input warning by ensuring default values in TextFields

--- Laboratorio Módulo 4.1 - Frameworks - React/Gestión de pedidos/src/pods/order-detail/header/order-detail-header.component.tsx
+++ Laboratorio Módulo 4.1 - Frameworks - React/Gestión de pedidos/src/pods/order-detail/header/order-detail-header.component.tsx
@@ -15,30 +15,30 @@ export const OrderDetailHeaderComponent: FC<Props> = (props) => {
       <Box sx={{ m: "50px", width: "1000px", display: "flex", gap: "30px" }}>
         <TextField
           disabled
-          value={selectedOrder?.orderNumber}
+          value={selectedOrder?.orderNumber ?? ""}
           label="Order Nº"
         ></TextField>
         <TextField
           disabled
-          value={selectedOrder?.supplier}
+          value={selectedOrder?.supplier ?? ""}
           label="Supplier"
         ></TextField>
         <TextField
           disabled
-          value={selectedOrder?.date}
+          value={selectedOrder?.date ?? ""}
           label="Date"
         ></TextField>
       </Box>
       <Box sx={{ m: "50px", width: "1000px", display: "flex", gap: "30px" }}>
         <TextField
           disabled
-          value={selectedOrder?.totalAmount}
+          value={selectedOrder?.totalAmount ?? ""}
           label="Total amount"
         ></TextField>
         <TextField
           disabled
           sx={{ width: "100px" }}
-          value={`${Math.round(statusPercentage)}%`}
+          value={`${Math.round(statusPercentage) ?? 0}%`}
           label="Status"
         ></TextField>
         <Button
