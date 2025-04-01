fe092baaad254db59bcdc06c19325377a4d298c8
Guste Gaubaite <219.guste@gmail.com>
2025-04-01 16:22:48 +0200

    Render selected order items in a table

--- Laboratorio Módulo 4.1 - Frameworks - React/Gestión de pedidos/src/pods/order-detail/detail/order-detail.component.tsx
+++ Laboratorio Módulo 4.1 - Frameworks - React/Gestión de pedidos/src/pods/order-detail/detail/order-detail.component.tsx
@@ -1,5 +1,39 @@
+import { Item } from "@/core/model";
+import { TableHeader } from "@/pods/table-header/table-header";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+} from "@mui/material";
 import { FC } from "react";
 
-export const OrderDetailComponent: FC = () => {
-  return <></>;
+interface Props {
+  items: Item[];
+}
+
+const headerElements: string[] = ["", "Status", "Description", "Amount"];
+
+export const OrderDetailComponent: FC<Props> = (props) => {
+  const { items } = props;
+  return (
+    <>
+      <TableContainer>
+        <Table sx={{ minWidth: 650 }}>
+          <TableHeader headers={headerElements} />
+          <TableBody>
+            {items.map((item) => (
+              <TableRow key={item.description}>
+                <TableCell></TableCell>
+                <TableCell>{item.status}</TableCell>
+                <TableCell>{item.description}</TableCell>
+                <TableCell>{item.amount} €</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
 };
--- Laboratorio Módulo 4.1 - Frameworks - React/Gestión de pedidos/src/pods/order-detail/detail/order-detail.container.tsx
+++ Laboratorio Módulo 4.1 - Frameworks - React/Gestión de pedidos/src/pods/order-detail/detail/order-detail.container.tsx
@@ -1,6 +1,15 @@
-import { FC } from "react";
+import { FC, useContext, useEffect, useState } from "react";
 import { OrderDetailComponent } from "./order-detail.component";
+import { OrdersContext } from "@/core/context/orders-context";
+import { Item } from "@/core/model";
 
 export const OrderDetailContainer: FC = () => {
-  return <OrderDetailComponent />;
+  const { selectedOrder } = useContext(OrdersContext);
+  const [items, setItems] = useState<Item[]>([]);
+
+  useEffect(() => {
+    if (selectedOrder) setItems(selectedOrder?.items);
+  }, [selectedOrder]);
+
+  return <OrderDetailComponent items={items} />;
 };
