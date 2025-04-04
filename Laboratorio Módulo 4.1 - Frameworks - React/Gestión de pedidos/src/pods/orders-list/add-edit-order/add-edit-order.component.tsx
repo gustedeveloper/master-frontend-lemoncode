2239836e25e588aec84693ec195f8323ae624941
Guste Gaubaite <219.guste@gmail.com>
2025-04-04 18:59:22 +0200

    Establish add edit order component basic visual structure

--- Laboratorio Módulo 4.1 - Frameworks - React/Gestión de pedidos/src/pods/orders-list/add-edit-order/add-edit-order.component.tsx
+++ Laboratorio Módulo 4.1 - Frameworks - React/Gestión de pedidos/src/pods/orders-list/add-edit-order/add-edit-order.component.tsx
@@ -1,9 +1,58 @@
+import { Box, Button, TextField } from "@mui/material";
 import { FC } from "react";
 
 export const AddEditOrderComponent: FC = () => {
   return (
     <>
-      <h1>Add Edit Order component</h1>
+      <Box
+        sx={{
+          m: "50px",
+          display: "flex",
+          flexDirection: "column",
+          gap: "20px",
+        }}
+      >
+        <Button sx={{ width: "200px" }} variant="contained">
+          Add new order
+        </Button>
+        <TextField
+          type="text"
+          slotProps={{
+            inputLabel: {
+              shrink: true,
+            },
+          }}
+          label="Order Nº"
+        ></TextField>
+        <TextField
+          type="text"
+          slotProps={{
+            inputLabel: {
+              shrink: true,
+            },
+          }}
+          label="Supplier"
+        ></TextField>
+        <TextField
+          type="date"
+          slotProps={{
+            inputLabel: {
+              shrink: true,
+            },
+          }}
+          label="Date"
+          placeholder=""
+        ></TextField>
+        <TextField
+          disabled
+          slotProps={{
+            inputLabel: {
+              shrink: true,
+            },
+          }}
+          label="Total amount"
+        ></TextField>
+      </Box>
     </>
   );
 };
