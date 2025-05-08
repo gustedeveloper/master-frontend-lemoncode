86ebb5f2522295268abe401393f4f0db66036a48
Guste Gaubaite <219.guste@gmail.com>
2025-05-08 20:04:49 +0200

    Create and style public header

---
+++ Laboratorio Módulo 4.2 - Frameworks - Angular/layout-mini-app/src/app/layout/header-public/header-public.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-public',
+  imports: [],
+  templateUrl: './header-public.component.html',
+  styleUrl: './header-public.component.scss',
+})
+export class HeaderPublicComponent {}
