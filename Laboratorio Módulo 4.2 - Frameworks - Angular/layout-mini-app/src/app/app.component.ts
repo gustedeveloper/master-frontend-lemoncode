57d69a5337f9be3f4ef18106b364deededd978e9
Guste Gaubaite <219.guste@gmail.com>
2025-05-08 20:12:50 +0200

    Add components imports

--- Laboratorio Módulo 4.2 - Frameworks - Angular/layout-mini-app/src/app/app.component.ts
+++ Laboratorio Módulo 4.2 - Frameworks - Angular/layout-mini-app/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderPublicComponent } from './layout/header-public/header-public.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderPublicComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
