f8a7b780c8a624342e0ef7e430904886feedeeb1
Guste Gaubaite <219.guste@gmail.com>
2025-05-08 20:13:42 +0200

    Establish public routes, import router to public header component

--- Laboratorio Módulo 4.2 - Frameworks - Angular/layout-mini-app/src/app/layout/header-public/header-public.component.ts
+++ Laboratorio Módulo 4.2 - Frameworks - Angular/layout-mini-app/src/app/layout/header-public/header-public.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLinkActive } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header-public',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header-public.component.html',
   styleUrl: './header-public.component.scss',
 })
