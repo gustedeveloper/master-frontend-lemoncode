f6cb0e67d46a0de29d7581e384c70915d1482b36
Guste Gaubaite <219.guste@gmail.com>
2025-05-09 18:49:11 +0200

    Add router to private menu component to activate navigation

--- Laboratorio Módulo 4.2 - Frameworks - Angular/layout-mini-app/src/app/layout/private-menu/private-menu.component.ts
+++ Laboratorio Módulo 4.2 - Frameworks - Angular/layout-mini-app/src/app/layout/private-menu/private-menu.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLinkActive } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-private-menu',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './private-menu.component.html',
-  styleUrl: './private-menu.component.scss'
+  styleUrl: './private-menu.component.scss',
 })
-export class PrivateMenuComponent {
-
-}
+export class PrivateMenuComponent {}
