33026bfa78099b86a85d98cb7a4d1552e861d41e
Guste Gaubaite <219.guste@gmail.com>
2025-05-09 18:56:02 +0200

    Add router to home component and style button a element

--- Laboratorio Módulo 4.2 - Frameworks - Angular/layout-mini-app/src/app/pages/home/home.component.ts
+++ Laboratorio Módulo 4.2 - Frameworks - Angular/layout-mini-app/src/app/pages/home/home.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-home',
-  imports: [MatButtonModule, MatIconModule],
+  imports: [MatButtonModule, MatIconModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
