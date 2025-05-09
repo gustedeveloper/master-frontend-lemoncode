306653334ff008c994417b635090ec19b0b61ed1
Guste Gaubaite <219.guste@gmail.com>
2025-05-09 17:52:20 +0200

    Add dashboard route

--- Laboratorio Módulo 4.2 - Frameworks - Angular/layout-mini-app/src/app/app.routes.ts
+++ Laboratorio Módulo 4.2 - Frameworks - Angular/layout-mini-app/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AboutComponent } from './about/about.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
