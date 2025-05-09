cc15744b869f37c09726835aa240ad16e4b81b80
Guste Gaubaite <219.guste@gmail.com>
2025-05-09 16:23:36 +0200

    Create login structure, style with angular material and animations

--- Laboratorio Módulo 4.2 - Frameworks - Angular/layout-mini-app/src/app/login/login.component.ts
+++ Laboratorio Módulo 4.2 - Frameworks - Angular/layout-mini-app/src/app/login/login.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}
