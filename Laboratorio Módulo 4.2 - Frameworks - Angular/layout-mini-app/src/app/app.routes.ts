5988bddb9771be5a5b18b1db78aba2e4dabba6f4
Guste Gaubaite <219.guste@gmail.com>
2025-05-09 18:48:50 +0200

    Add routes to create, gallery and profile pages

--- Laboratorio Módulo 4.2 - Frameworks - Angular/layout-mini-app/src/app/app.routes.ts
+++ Laboratorio Módulo 4.2 - Frameworks - Angular/layout-mini-app/src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AboutComponent } from './about/about.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CreateComponent } from './create/create.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ProfileComponent } from './profile/profile.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -10,4 +13,7 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'about', component: AboutComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'create', component: CreateComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'profile', component: ProfileComponent },
 ];
