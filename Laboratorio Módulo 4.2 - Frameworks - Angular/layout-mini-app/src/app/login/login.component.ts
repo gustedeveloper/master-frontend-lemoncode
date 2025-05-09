3fa27ed326ac54c0166589b8262d754f000d05ff
Guste Gaubaite <219.guste@gmail.com>
2025-05-09 17:57:45 +0200

    Connect form with AuthService, navigate to dashboard on success

--- Laboratorio Módulo 4.2 - Frameworks - Angular/layout-mini-app/src/app/login/login.component.ts
+++ Laboratorio Módulo 4.2 - Frameworks - Angular/layout-mini-app/src/app/login/login.component.ts
@@ -1,11 +1,58 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { AuthService } from '../core/auth.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-login',
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  error = false;
+  loginForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(4)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const { username, password } = this.loginForm.value;
+      const success = this.auth.login({
+        username: username!,
+        password: password!,
+      });
+
+      if (success) {
+        this.router.navigate(['/dashboard']);
+      } else {
+        this.error = true;
+      }
+    }
+  }
+}
