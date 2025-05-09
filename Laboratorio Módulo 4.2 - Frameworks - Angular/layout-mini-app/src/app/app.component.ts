07fd80cffd946ebad42c7f7a433b78e3a8ea74be
Guste Gaubaite <219.guste@gmail.com>
2025-05-09 18:40:35 +0200

    Update imports and add authservice to app

--- Laboratorio Módulo 4.2 - Frameworks - Angular/layout-mini-app/src/app/app.component.ts
+++ Laboratorio Módulo 4.2 - Frameworks - Angular/layout-mini-app/src/app/app.component.ts
@@ -2,13 +2,25 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderPublicComponent } from './layout/header-public/header-public.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderPrivateComponent } from './layout/header-private/header-private.component';
+import { CommonModule } from '@angular/common';
+import { AuthService } from './core/auth.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderPublicComponent, FooterComponent],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    HeaderPublicComponent,
+    HeaderPrivateComponent,
+    FooterComponent,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'layout-mini-app';
+
+  constructor(public authService: AuthService) {}
 }
