0b651ff1efdf6863f24dba528239612b6b3833d1
Guste Gaubaite <219.guste@gmail.com>
2025-05-12 13:54:27 +0200

    Implement mat menu to public and private menus in small devices, adjust headers and styles for according elements responsivness

--- Laboratorio Módulo 4.2 - Frameworks - Angular/layout-mini-app/src/app/layout/private-menu/private-menu.component.ts
+++ Laboratorio Módulo 4.2 - Frameworks - Angular/layout-mini-app/src/app/layout/private-menu/private-menu.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterLinkActive } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { Router, RouterLinkActive } from '@angular/router';
 import { RouterLink } from '@angular/router';
-
+import { MatMenuModule } from '@angular/material/menu';
+import { AuthService } from '../../core/auth.service';
 @Component({
   selector: 'app-private-menu',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './private-menu.component.html',
   styleUrl: './private-menu.component.scss',
 })
-export class PrivateMenuComponent {}
+export class PrivateMenuComponent {
+  username: string = '';
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.username = this.authService.getUsername();
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/home']);
+  }
+}
--- Laboratorio Módulo 4.2 - Frameworks - Angular/layout-mini-app/src/app/layout/public-menu/public-menu.component.ts
+++ Laboratorio Módulo 4.2 - Frameworks - Angular/layout-mini-app/src/app/layout/public-menu/public-menu.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterLinkActive } from '@angular/router';
 import { RouterLink } from '@angular/router';
-
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-public-menu',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './public-menu.component.html',
   styleUrl: './public-menu.component.scss',
 })
