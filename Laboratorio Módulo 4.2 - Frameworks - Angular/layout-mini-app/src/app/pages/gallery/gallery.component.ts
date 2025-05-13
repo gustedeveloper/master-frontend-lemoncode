0f0f407e16d50c8fc5191dc235efb13a945256ce
Guste Gaubaite <219.guste@gmail.com>
2025-05-13 12:20:22 +0200

    Add memory card component to gallery to display all memories

--- Laboratorio Módulo 4.2 - Frameworks - Angular/layout-mini-app/src/app/pages/gallery/gallery.component.ts
+++ Laboratorio Módulo 4.2 - Frameworks - Angular/layout-mini-app/src/app/pages/gallery/gallery.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Memory } from '../../core/memory/memory.model';
+import { MemoryService } from '../../core/memory/memory.service';
+import { MemoryCardComponent } from '../../shared/memory-card/memory-card.component';
+import { NgFor } from '@angular/common';
+import { MatGridListModule } from '@angular/material/grid-list';
 @Component({
   selector: 'app-gallery',
-  imports: [],
+  imports: [MemoryCardComponent, NgFor, MatGridListModule],
   templateUrl: './gallery.component.html',
-  styleUrl: './gallery.component.scss'
+  styleUrl: './gallery.component.scss',
 })
-export class GalleryComponent {
+export class GalleryComponent implements OnInit {
+  memories: Memory[] = [];
 
+  constructor(private memoryService: MemoryService) {}
+  ngOnInit(): void {
+    this.memoryService.getMemories().subscribe((memories) => {
+      this.memories = memories;
+    });
+  }
 }
