8f4b44a704b437ad7712ce96679500035788d5eb
Guste Gaubaite <219.guste@gmail.com>
2025-05-13 11:45:51 +0200

    Create Memory interface and mock data for initial memories list

---
+++ Laboratorio Módulo 4.2 - Frameworks - Angular/layout-mini-app/src/app/core/memory/memory.mock.ts
@@ -0,0 +1,69 @@
+import { Memory } from './memory.model';
+
+export const mockMemories: Memory[] = [
+  {
+    id: '1',
+    title: 'Amanecer entre gigantes',
+    date: '2024-09-22',
+    description: 'Desayuno en la furgoneta frente a las montañas al amanecer.',
+    emoji: ['⛰️', '☀️', '😊'],
+    imageUrl: 'memories/image1.jpg',
+  },
+  {
+    id: '2',
+    title: 'Atardecer con malvaviscos',
+    date: '2023-08-10',
+    description: 'Tarde de senderismo y marshmallows con amigas en la cima.',
+    emoji: ['🔥', '🥰', '🌄'],
+    imageUrl: 'memories/image2.jpg',
+  },
+  {
+    id: '3',
+    title: 'Picnic al atardecer',
+    date: '2022-06-01',
+    description: 'Un picnic improvisado con risas, música y luz dorada.',
+    emoji: ['🍇', '🧀', '🧺'],
+    imageUrl: 'memories/image3.jpg',
+  },
+  {
+    id: '4',
+    title: 'Caminata por el bosque',
+    date: '2023-07-15',
+    description:
+      'Explorando senderos con mochila a la espalda y buena compañía.',
+    emoji: ['🌲', '🥾', '💚'],
+    imageUrl: 'memories/image4.jpg',
+  },
+  {
+    id: '5',
+    title: 'Refugio entre cafés',
+    date: '2022-12-05',
+    description: 'Café caliente, muffins caseros y lluvia tras los cristales.',
+    emoji: ['☕', '📖', '🍂'],
+    imageUrl: 'memories/image5.jpg',
+  },
+  {
+    id: '6',
+    title: 'Acantilados del fin del mundo en Escocia',
+    date: '2023-05-19',
+    description: 'Un faro, el viento, el mar golpeando y la inmensidad.',
+    emoji: ['🌊', '🌬️', '🗺️'],
+    imageUrl: 'memories/image6.jpg',
+  },
+  {
+    id: '7',
+    title: 'Paseo por un cuento de hadas en Edimburgo',
+    date: '2023-09-10',
+    description: 'Calles medievales, río tranquilo y casas de colores.',
+    emoji: ['🏰', '🍃', '🌧️'],
+    imageUrl: 'memories/image7.jpg',
+  },
+  {
+    id: '8',
+    title: 'Nuestra guarida sobre ruedas',
+    date: '2024-04-14',
+    description: 'Vistas épicas desde la cama de nuestra furgoneta',
+    emoji: ['🛻', '🧦', '💛'],
+    imageUrl: 'memories/image8.jpg',
+  },
+];
---
+++ Laboratorio Módulo 4.2 - Frameworks - Angular/layout-mini-app/src/app/core/memory/memory.model.ts
@@ -0,0 +1,8 @@
+export interface Memory {
+  id: string;
+  title: string;
+  date: string;
+  description: string;
+  emoji: string[];
+  imageUrl: string;
+}
