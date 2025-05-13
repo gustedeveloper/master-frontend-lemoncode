b0be48641d3fe85cb6a4063245bbf13dc875c7ee
Guste Gaubaite <219.guste@gmail.com>
2025-05-13 11:49:04 +0200

    Implement memory card component to dashboard and display 3 latest user memories

--- Laboratorio Módulo 4.2 - Frameworks - Angular/layout-mini-app/src/app/pages/dashboard/dashboard.component.ts
+++ Laboratorio Módulo 4.2 - Frameworks - Angular/layout-mini-app/src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../core/auth.service';
+import { Router } from '@angular/router';
+import { Memory } from '../../core/memory/memory.model';
+import { MemoryService } from '../../core/memory/memory.service';
+import { MemoryCardComponent } from '../../shared/memory-card/memory-card.component';
+import { NgFor } from '@angular/common';
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [MemoryCardComponent, NgFor],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  username: string = '';
+  memories: Memory[] = [];
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private memoryService: MemoryService
+  ) {}
 
+  ngOnInit(): void {
+    this.username = this.authService.getUsername();
+    this.memoryService.getMemories().subscribe((memories) => {
+      this.memories = memories.slice(-3);
+    });
+  }
 }
