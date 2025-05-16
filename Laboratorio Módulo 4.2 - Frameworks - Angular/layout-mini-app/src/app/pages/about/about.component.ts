627599101f0f7bc83be6c8b75e7a0e2d3a4c5374
Guste Gaubaite <219.guste@gmail.com>
2025-05-16 20:19:08 +0200

    Implementation of about content and its styling

--- Laboratorio Módulo 4.2 - Frameworks - Angular/layout-mini-app/src/app/pages/about/about.component.ts
+++ Laboratorio Módulo 4.2 - Frameworks - Angular/layout-mini-app/src/app/pages/about/about.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
 })
-export class AboutComponent {
-
-}
+export class AboutComponent {}
