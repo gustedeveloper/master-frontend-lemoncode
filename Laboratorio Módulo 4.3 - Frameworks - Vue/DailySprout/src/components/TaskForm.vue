1de3b723c5dc4d6d70aba76379f2bc538887297d
Guste Gaubaite <219.guste@gmail.com>
2025-05-30 21:30:30 +0200

    Add extended task form with status and flower selection

--- Laboratorio Módulo 4.3 - Frameworks - Vue/DailySprout/src/components/TaskForm.vue
+++ Laboratorio Módulo 4.3 - Frameworks - Vue/DailySprout/src/components/TaskForm.vue
@@ -1,13 +1,23 @@
 <script setup lang="ts">
 import { useTasksStore } from '@/stores/tasks'
-import { ref } from 'vue'
+import FlowerSelect from '@/common/FlowerSelect.vue'
+import { reactive } from 'vue'
+import { flowers } from '@/utils/flower.util'
 
-const newTaskTitle = ref('')
+const defaultFlower = flowers[0] || ''
+
+let newTask = reactive({
+  title: '',
+  status: 'Just started!',
+  selectedFlower: defaultFlower,
+})
 
 const addTask = () => {
-  if (newTaskTitle.value.trim()) {
-    tasks.addTask(newTaskTitle.value)
-    newTaskTitle.value = ''
+  if (newTask.title.trim()) {
+    tasks.addTask({ ...newTask })
+    newTask.title = ''
+    newTask.status = 'Just started!'
+    newTask.selectedFlower = defaultFlower
   }
 }
 
@@ -15,10 +25,53 @@ const tasks = useTasksStore()
 </script>
 
 <template>
-  <form @submit.prevent="addTask">
-    <input v-model="newTaskTitle" placeholder="Write your task" />
+  <form class="form-container" @submit.prevent="addTask">
+    <input class="title-input" v-model="newTask.title" placeholder="Write your task" />
+    <div class="task-selection-options">
+      <div class="status-selection">
+        <label for="status-select">Choose task status: </label>
+        <select id="status-select" v-model="newTask.status">
+          <option value="Just started!">Just started!</option>
+          <option value="In progress!">In progress!</option>
+          <option value="Completed!">Completed!</option>
+        </select>
+      </div>
+      <div class="flower-selector">
+        <label>Select a flower:</label>
+        <FlowerSelect v-model="newTask.selectedFlower" />
+      </div>
+    </div>
     <button>Add task</button>
   </form>
 </template>
 
-<style scoped></style>
+<style scoped>
+.form-container {
+  margin: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  .title-input {
+    width: 400px;
+  }
+
+  .task-selection-options {
+    display: flex;
+    gap: 20px;
+  }
+
+  #status-select {
+    width: 100px;
+  }
+
+  .flower-selector {
+    display: flex;
+    gap: 5px;
+  }
+
+  button {
+    width: 100px;
+  }
+}
+</style>
