44bc8b84038ce23c35ab2e584a2e4a91c698b76d
Guste Gaubaite <219.guste@gmail.com>
2025-06-05 10:57:05 +0200

    Style nav bar to meet pixel art style

--- Laboratorio Módulo 4.3 - Frameworks - Vue/DailySprout/src/App.vue
+++ Laboratorio Módulo 4.3 - Frameworks - Vue/DailySprout/src/App.vue
@@ -5,8 +5,9 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
     <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/" class="nav-link pixel-button">Home</RouterLink>
+
+      <RouterLink to="/about" class="nav-link pixel-button">About</RouterLink>
     </nav>
   </header>
 
@@ -18,9 +19,38 @@ body {
   margin: 0;
   background-image: url('/backgrounds/bg.png');
   background-size: cover;
-
   background-repeat: no-repeat;
   height: 100vh;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+
+nav {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 50px;
+  gap: 50px;
+  margin-bottom: 50px;
+}
+
+.nav-link {
+  height: 50px;
+  width: 100px;
+  font-size: 20px;
+  text-align: center;
+  background-color: var(--pixel-tertiary-button-bg);
+  border: 3px solid var(--pixel-tertiary-button-border);
+  text-decoration: none;
+  color: var(--pixel-tertiary-button-border);
+}
+
+.nav-link:hover {
+  background-color: var(--pixel-tertiary-button-hover);
 }
 </style>
 
