cd4159f1975d7f9feb9d21f11d0e8641a66b45ca
Guste Gaubaite <219.guste@gmail.com>
2025-06-06 13:31:59 +0200

    chore: include additional flower image in flower list (15 total)

--- Laboratorio Módulo 4.3 - Frameworks - Vue/DailySprout/src/utils/flower.util.ts
+++ Laboratorio Módulo 4.3 - Frameworks - Vue/DailySprout/src/utils/flower.util.ts
@@ -1,4 +1,4 @@
-const flowerPngFactory = (count = 14, basePath = '/flowers') =>
+const flowerPngFactory = (count = 15, basePath = '/flowers') =>
   Array.from({ length: count }, (_, i) => `${basePath}/flower${i}.png`)
 
 export const flowers = flowerPngFactory()
